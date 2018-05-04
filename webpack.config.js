6ae3d1326edc2a17390feeb530eb570f6b5cdbe4
Ali Almohaya <AliAlmohaya@gmail.com>
2018-05-04 15:55:54 +0300

    extract css files outside the bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,30 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    entry: path.join(__dirname, 'src/app.js'),
+    entry: {
+        app: path.join(__dirname, 'src/app.js')
+    },
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist')
     },
     mode: 'development',
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[id].css',
+            chunkFilename: "[id].css"
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ],
     module: {
         rules: [{
                 test: /\.s[ac]ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.css$/,
